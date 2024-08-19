7c3acf8487d3852012083ce81bd132b0d5f21c11
Reyad <reyad@skylarkcreative.co.uk>
2024-08-19 17:32:40 +0600

    added jquery code for expand card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,32 @@
-const expandItem = document.querySelectorAll(".expanding_item");
+// conflict bug solution
+// var $ = jQuery.noConflict();
 
-expandItem.forEach((item) => {
-  item.addEventListener("mouseover", () => {
+// $(function () {
+  const expandItem = document.querySelectorAll(".expanding_item");
+
+  expandItem.forEach((item) => {
+    item.addEventListener("mouseover", () => {
       removeActiveClass();
       item.classList.add("active");
+    });
   });
-});
 
-function removeActiveClass() {
-  expandItem.forEach((item) => {
-    item.classList.remove("active");
-  });
-}
+  function removeActiveClass() {
+    expandItem.forEach((item) => {
+      item.classList.remove("active");
+    });
+  }
+
+  // *********** expand using jquey ************
+  // const $expandItem = $(".expanding_item");
+  // $expandItem.on("mouseover", function() {
+  //   removeActiveClass();
+  //   $(this).addClass("active");
+  // });
+
+  // function removeActiveClass() {
+  //   $expandItem.removeClass("active");
+  // }
+
+
+// });
